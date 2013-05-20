4f32496c6fc18a4b9454b7ea4da2324755ae9456
Christian Lück <christian@lueck.tv>
2013-05-20 10:13:09 +0200

    Add support for psr-0 autoloader namespace array

--- src/Clue/PharComposer/Bundler/Explicit.php
+++ src/Clue/PharComposer/Bundler/Explicit.php
@@ -17,24 +17,30 @@ class Explicit extends Base
 
         if ($autoload !== null) {
             if (isset($autoload['psr-0'])) {
-                foreach ($autoload['psr-0'] as $namespace => $path) {
-                    if ($namespace !== '') {
-                        // namespace given, add it to the path
-                        $namespace = str_replace('\\', '/', $namespace);
-
-                        if ($path === '') {
-                            // namespace in project root => namespace is path
-                            $path = $namespace;
-                        } else {
-                            // namespace in sub-directory => add namespace to path
-                            $path = rtrim($path, '/') . '/' . $namespace;
-                        }
+                foreach ($autoload['psr-0'] as $namespace => $paths) {
+                    if (!is_array($paths)) {
+                        // PSR autoloader may define a single or multiple paths
+                        $paths = array($paths);
                     }
+                    foreach($paths as $path) {
+                        if ($namespace !== '') {
+                            // namespace given, add it to the path
+                            $namespace = str_replace('\\', '/', $namespace);
 
-                    // TODO: this is not correct actually... should work for most repos nevertheless
-                    // TODO: we have to take target-dir into account
+                            if ($path === '') {
+                                // namespace in project root => namespace is path
+                                $path = $namespace;
+                            } else {
+                                // namespace in sub-directory => add namespace to path
+                                $path = rtrim($path, '/') . '/' . $namespace;
+                            }
+                        }
 
-                    $this->addDirectory($this->pharcomposer->getAbsolutePathForComposerPath($path));
+                        // TODO: this is not correct actually... should work for most repos nevertheless
+                        // TODO: we have to take target-dir into account
+
+                        $this->addDirectory($this->pharcomposer->getAbsolutePathForComposerPath($path));
+                    }
                 }
             }
 
