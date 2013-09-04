47107d838199da3d73dc1b5c94eb8d8fcaa1459b
Christian Lück <clueck@datus.com>
2013-09-04 13:59:00 +0200

    Add default command name 'search' it nothing was given

--- src/Clue/PharComposer/App.php
+++ src/Clue/PharComposer/App.php
@@ -3,9 +3,12 @@
 namespace Clue\PharComposer;
 
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputInterface;
 
 class App extends BaseApplication
 {
+    private $isDefault = false;
+
     public function __construct()
     {
         parent::__construct('phar-composer', '@git_tag@');
@@ -14,5 +17,42 @@ class App extends BaseApplication
         $this->add(new Command\Search());
         $this->add(new Command\Install());
         $this->add(new Command\Gui());
+    }
+
+    /**
+     * Gets the name of the command based on input.
+     *
+     * @param InputInterface $input The input interface
+     *
+     * @return string The command name
+     */
+    protected function getCommandName(InputInterface $input)
+    {
+        if ($input->getFirstArgument() === null && !$input->hasParameterOption(array('--help', '-h'))) {
+            $this->isDefault = true;
+            return $this->getDefaultCommandName();
+        }
+        return parent::getCommandName($input);
+    }
+
+    private function getDefaultCommandName()
+    {
+        return 'search';
+    }
+
+    /**
+     * Overridden so that the application doesn't expect the command
+     * name to be the first argument.
+     */
+    public function getDefinition()
+    {
+        $inputDefinition = parent::getDefinition();
+
+        if ($this->isDefault) {
+            // clear out the normal first argument, which is the command name
+            $inputDefinition->setArguments();
+        }
+
+        return $inputDefinition;
     }
 }
