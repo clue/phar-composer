1ecab0446d2a00181ed99e73b9effa025c47b296
mikey179 <mikey@stubbles.net>
2013-10-26 23:28:47 +0200

    refactor to break large method into smaller ones

--- src/Clue/PharComposer/Bundler/Explicit.php
+++ src/Clue/PharComposer/Bundler/Explicit.php
@@ -15,49 +15,69 @@ class Explicit extends Base
         $autoload = $this->package->getAutoload();
 
         if ($autoload !== null) {
-            if (isset($autoload['psr-0'])) {
-                foreach ($autoload['psr-0'] as $namespace => $paths) {
-                    if (!is_array($paths)) {
-                        // PSR autoloader may define a single or multiple paths
-                        $paths = array($paths);
-                    }
-                    foreach($paths as $path) {
-                        if ($namespace !== '') {
-                            // namespace given, add it to the path
-                            $namespace = str_replace('\\', '/', $namespace);
+            $this->bundlePsr0($autoload);
+            $this->bundleClassmap($autoload);
+            $this->bundleFiles($autoload);
+        }
+    }
 
-                            if ($path === '') {
-                                // namespace in project root => namespace is path
-                                $path = $namespace;
-                            } else {
-                                // namespace in sub-directory => add namespace to path
-                                $path = rtrim($path, '/') . '/' . $namespace;
-                            }
-                        }
+    private function bundlePsr0(array $autoload)
+    {
+        if (!isset($autoload['psr-0'])) {
+            return;
+        }
 
-                        // TODO: this is not correct actually... should work for most repos nevertheless
-                        // TODO: we have to take target-dir into account
-
-                        $this->addDirectory($this->package->getAbsolutePath($path));
-                    }
-                }
+        foreach ($autoload['psr-0'] as $namespace => $paths) {
+            if (!is_array($paths)) {
+                // PSR autoloader may define a single or multiple paths
+                $paths = array($paths);
             }
+            foreach($paths as $path) {
+                // TODO: this is not correct actually... should work for most repos nevertheless
+                // TODO: we have to take target-dir into account
 
-            if (isset($autoload['classmap'])) {
-                foreach($autoload['classmap'] as $path) {
-                    $path = $this->package->getAbsolutePath($path);
-                    if (is_dir($path)) {
-                        $this->addDirectory($path);
-                    } else {
-                        $this->addFile($path);
-                    }
-                }
+                $this->addDirectory($this->package->getAbsolutePath($this->buildNamespacePath($namespace, $path)));
             }
+        }
+    }
 
-            if (isset($autoload['files'])) {
-                foreach($autoload['files'] as $path) {
-                    $this->addFile($this->package->getAbsolutePath($path));
-                }
+    private function buildNamespacePath($namespace, $path)
+    {
+        if ($namespace === '') {
+            return $path;
+        }
+
+        $namespace = str_replace('\\', '/', $namespace);
+        if ($path === '') {
+            // namespace in project root => namespace is path
+            return $namespace;
+        }
+
+        // namespace in sub-directory => add namespace to path
+        return rtrim($path, '/') . '/' . $namespace;
+    }
+
+    private function bundleClassmap(array $autoload)
+    {
+        if (!isset($autoload['classmap'])) {
+            return;
+        }
+
+        foreach($autoload['classmap'] as $path) {
+            $path = $this->package->getAbsolutePath($path);
+            if (is_dir($path)) {
+                $this->addDirectory($path);
+            } else {
+                $this->addFile($path);
+            }
+        }
+    }
+
+    private function bundleFiles(array $autoload)
+    {
+        if (isset($autoload['files'])) {
+            foreach($autoload['files'] as $path) {
+                $this->addFile($this->package->getAbsolutePath($path));
             }
         }
     }
