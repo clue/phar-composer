98650b75dd64a49485ab38ac1c81a8a50342924a
mikey179 <mikey@stubbles.net>
2013-12-12 08:37:21 +0100

    introduce abstraction for phar to be created

---
+++ src/Clue/PharComposer/TargetPhar.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Clue\PharComposer;
+
+use Herrera\Box\Box;
+use Traversable;
+/**
+ * Represents the target phar to be created.
+ *
+ * TODO: replace PharComposer with a new BasePath class
+ */
+class TargetPhar
+{
+    /**
+     *
+     * @type  PharComposer
+     */
+    private $pharComposer;
+    /**
+     *
+     * @type  Box
+     */
+    private $box;
+
+    /**
+     * constructor
+     *
+     * @param  Box           $box
+     * @param  PharComposer  $pharComposer
+     */
+    public function __construct(Box $box, PharComposer $pharComposer)
+    {
+        $this->box = $box;
+        $this->box->getPhar()->startBuffering();
+        $this->pharComposer = $pharComposer;
+    }
+
+    /**
+     * create new instance in target path
+     *
+     * @param   string        $target
+     * @param   PharComposer  $pharComposer
+     * @return  TargetPhar
+     */
+    public static function create($target, PharComposer $pharComposer)
+    {
+        return new self(Box::create($target), $pharComposer);
+    }
+
+    /**
+     * finalize writing of phar file
+     */
+    public function finalize()
+    {
+        $this->box->getPhar()->stopBuffering();
+    }
+
+    public function addFromString($local, $contents)
+    {
+        $this->box->addFromString($local, $contents);
+    }
+
+     /**
+     * Adds a file to the Phar, after compacting it and replacing its
+     * placeholders.
+     *
+     * @param string $file  The file name.
+     */
+    public function addFile($file)
+    {
+        $this->box->addFile($file, $this->pharComposer->getPathLocalToBase($file));
+    }
+
+     /**
+     * Similar to Phar::buildFromIterator(), except the files will be compacted
+     * and their placeholders replaced.
+     *
+     * @param Traversable $iterator The iterator.
+     */
+    public function buildFromIterator(Traversable $iterator)
+    {
+        $this->box->buildFromIterator($iterator, $this->pharComposer->getBase());
+    }
+
+    /**
+     * Used to set the PHP loader or bootstrap stub of a Phar archive
+     *
+     * @param  string $stub
+     */
+    public function setStub($stub)
+    {
+        $this->box->getPhar()->setStub($stub);
+    }
+}
---
+++ tests/TargetPharTest.php
@@ -0,0 +1,91 @@
+<?php
+
+use Clue\PharComposer\TargetPhar;
+
+class TargetPharTest extends TestCase
+{
+    /**
+     * instance to test
+     *
+     * @ype  TargetPhar
+     */
+    private $targetPhar;
+
+    private $mockPhar;
+
+    private $mockBox;
+
+    private $mockPharComposer;
+
+    /**
+     * set up test environment
+     */
+    public function setUp()
+    {
+        $this->mockPhar = $this->createMock('\Phar');
+        $this->mockBox  = $this->createMock('Herrera\Box\Box');
+        $this->mockBox->expects($this->any())
+                      ->method('getPhar')
+                      ->will($this->returnValue($this->mockPhar));
+        $this->mockPharComposer = $this->createMock('Clue\PharComposer\PharComposer');
+        $this->targetPhar       = new TargetPhar($this->mockBox, $this->mockPharComposer);
+    }
+
+    private function createMock($class)
+    {
+        return $this->getMockBuilder($class)
+                    ->disableOriginalConstructor()
+                    ->getMock();
+    }
+
+    /**
+     * @test
+     */
+    public function addFileCalculatesLocalPartForBox()
+    {
+        $this->mockPharComposer->expects($this->once())
+                               ->method('getPathLocalToBase')
+                               ->with($this->equalTo('path/to/package/file.php'))
+                               ->will($this->returnValue('file.php'));
+        $this->mockBox->expects($this->once())
+                      ->method('addFile')
+                      ->with($this->equalTo('path/to/package/file.php'), $this->equalTo('file.php'));
+        $this->targetPhar->addFile('path/to/package/file.php');
+    }
+
+    /**
+     * @test
+     */
+    public function buildFromIteratorProvidesBasePathForBox()
+    {
+        $mockTraversable = $this->getMock('\Iterator');
+        $this->mockPharComposer->expects($this->once())
+                               ->method('getBase')
+                               ->will($this->returnValue('path/to/package'));
+        $this->mockBox->expects($this->once())
+                      ->method('buildFromIterator')
+                      ->with($this->equalTo($mockTraversable), $this->equalTo('path/to/package'));
+        $this->targetPhar->buildFromIterator($mockTraversable);
+    }
+
+    /**
+     * @test
+     */
+    public function setsStubOnUnderlyingPhar()
+    {
+        $this->mockPhar->expects($this->once())
+                       ->method('setStub')
+                       ->with($this->equalTo('some stub code'));
+        $this->targetPhar->setStub('some stub code');
+    }
+
+    /**
+     * @test
+     */
+    public function finalizeStopsBufferingOnUnderlyingPhar()
+    {
+        $this->mockPhar->expects($this->once())
+                       ->method('stopBuffering');
+        $this->targetPhar->finalize();
+    }
+}
